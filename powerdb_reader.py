270597372ce54d173f516bdd94f91c9b751f90a1
Anurag Peshne <anurag.peshne@gmail.com>
2020-10-07 00:07:36 -0700

    initial working

---
+++ powerdb_reader.py
@@ -0,0 +1,54 @@
+import json
+import re
+
+subregex = re.compile(r"(\d):")
+def parse_powerdb_line(line):
+    timestamp, json_str = line.split(',', 1)
+
+    timestamp = int(timestamp) * 1000 # millisecond timestamp
+    double_quoted_json_str = json_str.strip("'<>() ").replace('\'', '\"')
+    valid_index_name_json_str = subregex.sub(r'"\1":', double_quoted_json_str)
+    if (len(valid_index_name_json_str) == 0):
+        return (timestamp, "")
+
+    parsed_json = json.loads(valid_index_name_json_str)
+    return (timestamp, parsed_json)
+
+class PowerDbReader:
+    def __init__(self, db_file_path):
+        self.total_plugs = 6
+        self.db = self.parse(db_file_path)
+
+    def parse(self, db_file_path):
+        parsed_data = []
+        with open(db_file_path, 'r') as db_file:
+            lines = [line.strip() for line in db_file]
+            for line in lines:
+                timestamp, reading = parse_powerdb_line(line)
+                if (len(reading) > 0):
+                    parsed_data.append((timestamp, reading))
+        print("parsed lines" + str(len(parsed_data)))
+        return parsed_data
+
+    def get_plug_names(self):
+        return ['Plug ' + str(i + 1) for i in range(self.total_plugs)]
+
+    def plug_name_to_id(self, name):
+        return str(int(name[-1]) - 1)
+
+    def get_data_in_range(self, target_plug, from_, to):
+        target_plug = str(target_plug)
+        i = 0
+        in_range_data = []
+        while i < len(self.db) and self.db[i][0] < from_:
+            i += 1
+        while i < len(self.db) and self.db[i][0] < to:
+            in_range_data.append(
+                [
+                    self.db[i][1][target_plug]['power_mw'],
+                    self.db[i][0]
+                ]
+            )
+            i += 1
+
+        return in_range_data
---
+++ server.py
@@ -0,0 +1,72 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from calendar import timegm
+from datetime import datetime
+import _strptime  # https://bugs.python.org/issue7980
+from flask import Flask, request, jsonify
+from powerdb_reader import PowerDbReader
+
+app = Flask(__name__)
+app.debug = True
+
+powerDbReader = None
+
+def convert_to_time_ms(timestamp):
+    return 1000 * timegm(datetime.strptime(timestamp, '%Y-%m-%dT%H:%M:%S.%fZ').timetuple())
+
+@app.route("/")
+def health_check():
+    return "ok"
+
+@app.route('/search', methods=['POST'])
+def search():
+    series = powerDbReader.get_plug_names()
+    return jsonify(series)
+
+@app.route('/query', methods=['POST'])
+def query():
+    req = request.get_json()
+
+    from_ = convert_to_time_ms(req['range']['from'])
+    to = convert_to_time_ms(req['range']['to'])
+
+    target = req['targets'][0]['target']
+    target_id = powerDbReader.plug_name_to_id(target)
+    data = [
+        {
+            "target": req['targets'][0]['target'],
+            "datapoints": powerDbReader.get_data_in_range(target_id, from_, to)
+        }
+    ]
+
+    return jsonify(data)
+
+@app.route('/annotations', methods=['POST'])
+def annotations():
+    req = request.get_json()
+    print(req)
+    data = [
+        {
+            "annotation": 'This is the annotation',
+            "time": (convert_to_time_ms(req['range']['from']) +
+                     convert_to_time_ms(req['range']['to'])) / 2,
+            "title": 'Deployment notes',
+            "tags": ['tag1', 'tag2'],
+            "text": 'Hm, something went wrong...'
+        }
+    ]
+    return jsonify(data)
+
+@app.after_request
+def after_request(response):
+    # CORS headers
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,POST,OPTIONS')
+    return response
+
+if __name__ == "__main__":
+    #TODO get powerdb file path as param
+    powerDbReader = PowerDbReader("/home/anurag/Sync/db/powerdb")
+    app.run()
