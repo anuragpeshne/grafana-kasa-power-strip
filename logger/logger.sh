8bfc37cde0de5eea4689a298521fa765d5400c98
Anurag Peshne <anpeshne@microsoft.com>
2021-06-24 18:59:09 -0700

    fixes logger.sh is var defined

--- logger/logger.sh
+++ logger/logger.sh
@@ -1,11 +1,10 @@
 #! /bin/bash
 
-if [ -z $STRIP_IP ];
-then 
+if [[ -z "${STRIP_IP}" ]]; then
+  echo "\$STRIP_IP not set" > /dev/stderr
+else
   power=$(/usr/local/bin/pyhs100 --strip --host $STRIP_IP emeter | tail -n 1)
   timestamp=$(date +%s)
 
   echo -e "$timestamp, $power"
-else
-  echo "\$STRIP_IP not set" > /dev/stderr
 fi
